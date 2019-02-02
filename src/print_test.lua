1c3ef5a06f6ce50fd0f7ce52c484378885bdbcb2
Youngbo Go <gyb357@naver.com>
2019-02-02 18:16:34 +0900

    Update print_test.lua

--- src/print_test.lua
+++ src/print_test.lua
@@ -6,3 +6,16 @@
 -- To change this template use File | Settings | File Templates.
 --
 
+array = {}
+array[1] = "a"
+array[2] = "b"
+array[3] = "c"
+
+function arrays(index)
+    print(array[index])
+end
+
+for i = 1, 3 do
+    arrays(1)
+end
+
